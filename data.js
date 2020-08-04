7b7005f53fd6f666163fbbf2112d5fdce93c2587
Mati <mattigravity9@gmail.com>
2020-08-04 18:21:32 +0200

    file: dataset example

---
+++ data.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+  return { 
+    products: [
+      { id: 1, name: "Buty adidas", category: "obuwie", description: "Wygodne buty sportowe", price: 240 },
+      { id: 2, name: "Kurtka sportowa", category: "kurtki", description: "Chroni przed deszczem", price: 250.99 },
+      { id: 3, name: "Spodenki", category: "Spodnie", description: "Stworzone z miękkiego materiału", price: 109.50 },
+      ],
+      orders: []
+  }
+}
