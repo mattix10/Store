ccef8e417de8507f4e818ad6225b191cc5f83afe
Mati <mattigravity9@gmail.com>
2020-08-06 14:30:35 +0200

    join RouterModule to store.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from './store/store.module';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    StoreModule
+    StoreModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
