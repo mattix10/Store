cab9e392fb18375ff26d394cdfcea5fe2fe10805
Mati <mattigravity9@gmail.com>
2020-08-06 11:57:41 +0200

    join Cart to model.module.ts

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ProductRepository } from './product.repository';
 import { DataSource } from './datasource';
+import { Cart } from './cart.model';
 
 @NgModule({
-  providers: [ProductRepository, DataSource]
+  providers: [ProductRepository, DataSource, Cart]
 })
 
 export class ModelModule {}
