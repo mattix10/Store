5bba0e6b668dc22a30851584691e286a30c5d7e0
Mati <mattigravity9@gmail.com>
2020-08-06 14:54:53 +0200

    join Order and OrderRepository to model.module

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { ProductRepository } from './product.repository';
 import { DataSource } from './datasource';
 import { Cart } from './cart.model';
+import { Order } from './order.model';
+import { OrderRepository } from './order.repository';
 
 @NgModule({
-  providers: [ProductRepository, DataSource, Cart]
+  providers: [ProductRepository, DataSource, Cart, Order, OrderRepository]
 })
 
 export class ModelModule {}
