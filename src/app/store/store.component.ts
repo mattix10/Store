349bf2d3ef1a4182e3bb973bda28b32c4673c2c6
Mati <mattigravity9@gmail.com>
2020-08-06 11:24:28 +0200

    modify: get products()

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -9,12 +9,15 @@ import { ProductRepository } from '../model/product.repository';
 
 export class StoreComponent {
   public selectedCategory = null;
-  public productsPerPage = 1;
+  public productsPerPage = 5;
+  public selectedPage = 1;
 
   constructor(private repository: ProductRepository) {}
 
   get products(): Product[] {
-    return this.repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+    return this.repository.getProducts(this.selectedCategory)
+    .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
@@ -28,5 +31,6 @@ export class StoreComponent {
   changePageSize(newSize: number) {
     this.productsPerPage = Number(newSize);
   }
+
 
 }
