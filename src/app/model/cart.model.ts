fbad0ae41a06f2fc012473a3ebf806d91f2f5354
Mati <mattigravity9@gmail.com>
2020-08-06 11:55:14 +0200

    feat: addLine()

--- src/app/model/cart.model.ts
+++ src/app/model/cart.model.ts
@@ -6,6 +6,17 @@ export class Cart {
   public itemCount = 0;
   public cartPrice = 0;
   public queues: CartLine[] = [];
+
+  addLine(product: Product, quantity: number = 1) {
+    const queue = this.queues.find(q => q.product.id === product.id);
+    if (queue !== undefined) {
+      queue.quantity += quantity;
+    } else {
+      this.queues.push(new CartLine(product, quantity));
+    }
+  }
 }
 
-export class CartLine {}
+export class CartLine {
+  constructor(public product: Product, public quantity: number) {}
+}
