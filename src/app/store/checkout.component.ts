0efbdbccf5e4b823ba72182f644b8898f092dd17
Mati <mattigravity9@gmail.com>
2020-08-06 15:09:23 +0200

    feat: submitOrder()

--- src/app/store/checkout.component.ts
+++ src/app/store/checkout.component.ts
@@ -1,7 +1,27 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Order } from '../model/order.model';
+import { OrderRepository } from '../model/order.repository';
+
 
 @Component({
   templateUrl: 'checkout.component.html'
 })
 
-export class CheckoutComponent {}
+export class CheckoutComponent {
+  orderSent = false;
+  submitted = false;
+
+  constructor(public repository: OrderRepository, public order: Order) {}
+
+  submitOrder(form: NgForm) {
+    this.submitted = true;
+    if (form.valid) {
+      this.repository.saveOrder(this.order).subscribe(order => {
+        this.order.clear();
+        this.orderSent = true;
+        this.submitted = false;
+      });
+    }
+  }
+}
