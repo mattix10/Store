0f8075e88508453b897eaad43cc51c28ebb0c36b
Mati <mattigravity9@gmail.com>
2020-08-07 18:09:14 +0200

    add path 'admin'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { StoreComponent } from './store/store.component';
 import { CheckoutComponent } from './store/checkout.component';
 import { CartDetailComponent } from './store/cartDetail.component';
 
-
 const routes: Routes = [
   { path: 'store', component: StoreComponent },
   { path: 'cart', component: CartDetailComponent },
   { path: 'checkout', component: CheckoutComponent },
+  { path: 'admin', loadChildren:  () => import('src/app/admin/admin.module').then(mod => mod.AdminModule)},
   { path: '**', redirectTo: '/store' }
 ];
 
