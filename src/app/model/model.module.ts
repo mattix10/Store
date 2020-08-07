7dab008718521529510ad443feb144a777e77856
Mati <mattigravity9@gmail.com>
2020-08-07 16:25:55 +0200

    join RestDataSource and HTTPModule to model.module.ts

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -4,9 +4,13 @@ import { DataSource } from './datasource';
 import { Cart } from './cart.model';
 import { Order } from './order.model';
 import { OrderRepository } from './order.repository';
+import { HttpClientModule } from '@angular/common/http';
+import { RestDataSource } from './rest.datasource';
 
 @NgModule({
-  providers: [ProductRepository, DataSource, Cart, Order, OrderRepository]
+  imports: [HttpClientModule],
+  providers: [ProductRepository, Cart, Order, OrderRepository, {
+    provide: DataSource, useClass: RestDataSource }]
 })
 
 export class ModelModule {}
