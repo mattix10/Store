12b800a418418ce6d4ec79bf2d1b19a00432ef44
Mati <mattigravity9@gmail.com>
2020-08-10 11:46:43 +0200

    feat: loadOrders() and getOrders()

--- src/app/model/order.repository.ts
+++ src/app/model/order.repository.ts
@@ -1,14 +1,29 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { DataSource } from './datasource';
+import { RestDataSource } from './rest.datasource';
 import { Order } from './order.model';
 
 @Injectable()
 export class OrderRepository {
+  private orders: Order[] = [];
+  private loaded = false;
 
-  constructor(private dataSource: DataSource) {}
+  constructor(private dataSource: RestDataSource) {}
 
   saveOrder(order: Order): Observable<Order> {
     return this.dataSource.saveOrder(order);
+  }
+
+  loadOrders() {
+    this.loaded = true;
+    this.dataSource.getOrders()
+      .subscribe(orders => this.orders = orders)
+  }
+
+  getOrders(): Order[] {
+    if (!this.loaded) {
+      this.loadOrders();
+    }
+    return this.orders;
   }
 }
