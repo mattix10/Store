8786325ea1fe0b23f7f7972815751e9c693798ba
Mati <mattigravity9@gmail.com>
2020-08-10 10:22:46 +0200

    feat: getProducts()

--- src/app/admin/productTable.component.ts
+++ src/app/admin/productTable.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
+import { ProductRepository } from '../model/product.repository';
+import { Product } from '../model/product.model';
 
 @Component({
   templateUrl: 'productTable.component.html'
 })
-export class ProductTableComponent {}
+export class ProductTableComponent {
+
+  constructor(private repository: ProductRepository) {}
+
+  getProducts(): Product[] {
+    return this.repository.getProducts();
+  }
+}
