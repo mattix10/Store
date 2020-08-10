b6816b452b2dc084d12d659c913bd58119c83294
Mati <mattigravity9@gmail.com>
2020-08-10 17:44:18 +0200

    adding file: model.resolver.ts

---
+++ src/app/model/model.resolver.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ProductRepository } from './product.repository';
+import { RestDataSource } from './rest.datasource';
+import { Product } from './product.model';
+
+@Injectable()
+export class ModelResolver {
+  constructor(private model: ProductRepository, private dataSource: RestDataSource) {}
+
+  resolve(route: ActivatedRouteSnapshot,
+          state: RouterStateSnapshot): Observable<Product[]> {
+             if (this.model.getProducts.length === 0) {
+              console.log('wczytuje');
+              return this.dataSource.getProducts();
+             }
+  }
+}
