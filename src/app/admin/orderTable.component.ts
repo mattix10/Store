c1c5e2e35f5b35e10542db4ca1f2b016224799b0
Mati <mattigravity9@gmail.com>
2020-08-10 15:02:17 +0200

    feat: markShipped()

--- src/app/admin/orderTable.component.ts
+++ src/app/admin/orderTable.component.ts
@@ -1,6 +1,23 @@
 import { Component } from '@angular/core';
+import { OrderRepository } from '../model/order.repository';
+import { Order } from '../model/order.model';
 
 @Component({
   templateUrl: 'orderTable.component.html'
 })
-export class OrderTableComponent {}
+export class OrderTableComponent {
+  showShipped = false;
+
+  constructor(private repository: OrderRepository) {}
+
+  getOrders(): Order[] {
+    console.log(this.repository.getOrders());
+    return this.repository.getOrders()
+      .filter(o => this.showShipped || !o.shipped);
+  }
+
+  markShipped(order: Order) {
+    order.shipped = true;
+    this.repository.updateOrder(order);
+  }
+}
