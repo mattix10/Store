004e625a9da7d9db824024b840fbc07be5057ea8
Mati <mattigravity9@gmail.com>
2020-08-10 17:45:56 +0200

    join ModelResolver to app-routing.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { StoreComponent } from './store/store.component';
 import { CheckoutComponent } from './store/checkout.component';
 import { CartDetailComponent } from './store/cartDetail.component';
+import { ModelResolver } from './model/model.resolver';
+
 
 const routes: Routes = [
-  { path: 'store', component: StoreComponent },
+  { path: 'store', component: StoreComponent, resolve: { model: ModelResolver}},
   { path: 'cart', component: CartDetailComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'admin', loadChildren:  () => import('src/app/admin/admin.module').then(mod => mod.AdminModule)},
