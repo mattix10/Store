5b0e60af05a9c0dc3e0945e931fb472467a259ec
Mati <mattigravity9@gmail.com>
2020-08-14 00:52:12 +0200

    join MaterialModule to store.module

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -7,15 +7,20 @@ import { CartSummaryComponent } from './cartSummary.component';
 import { CartDetailComponent } from './cartDetail.component';
 import { CheckoutComponent } from './checkout.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer.component';
+import { MaterialModule } from '../material.module';
+import { NavBarComponent } from './navbar.component';
 
 
 @NgModule({
-  imports: [ModelModule, BrowserModule, FormsModule, RouterModule],
+  imports: [ModelModule, BrowserModule, FormsModule, RouterModule, MaterialModule],
   declarations: [
     StoreComponent,
     CartSummaryComponent,
     CartDetailComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    FooterComponent,
+    NavBarComponent
   ],
   exports: [StoreComponent]
 })
