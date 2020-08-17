867322c328d579037ecd9f8f5e3a31fa7c2014d2
Mati <mattigravity9@gmail.com>
2020-08-17 17:22:50 +0200

    adding files: showImage.directive

---
+++ src/app/model/showImage.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, Renderer2, OnInit, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appShowImage]'
+})
+export class ShowImageDirective implements OnInit {
+
+  @Input('appShowImage')
+  product: any;
+  path: string;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.path = this.product.image;
+    const imgCard = this.el.nativeElement;
+    this.renderer.setAttribute(imgCard, 'src', `assets/${this.path}.jpg`);
+    }
+
+}
