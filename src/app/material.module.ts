fe22fcb47dc614d28555e856f5e81892f6c1af6a
Mati <mattigravity9@gmail.com>
2020-08-17 17:19:57 +0200

    join MatCardModule to material.module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatListModule } from '@angular/material/list';
-import {MatRippleModule} from '@angular/material/core';
+import { MatRippleModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [ CommonModule, MatButtonModule, MatGridListModule, MatListModule, MatRippleModule],
+  imports: [ CommonModule, MatButtonModule, MatGridListModule, MatListModule, MatRippleModule,
+             MatCardModule],
   declarations: [
   ],
-  exports: [MatButtonModule, MatGridListModule, MatListModule, MatRippleModule]
+  exports: [MatButtonModule, MatGridListModule, MatListModule, MatRippleModule, MatCardModule]
 })
 
 export class MaterialModule { }
