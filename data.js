afcd1864ce7841af75d011fb432adb06d064343c
Mati <mattigravity9@gmail.com>
2020-08-17 17:40:14 +0200

    add poperty 'image'

--- data.js
+++ data.js
@@ -1,9 +1,10 @@
 module.exports = function () {
   return { 
     products: [
-      { id: 1, name: "Buty adidas", category: "obuwie", description: "Wygodne buty sportowe", price: 240 },
-      { id: 2, name: "Kurtka sportowa", category: "kurtki", description: "Chroni przed deszczem", price: 250.99 },
-      { id: 3, name: "Spodenki", category: "Spodnie", description: "Stworzone z miękkiego materiału", price: 109.50 },
+      { id: 1, name: "Buty adidas", category: "obuwie", description: "Wygodne buty sportowe", price: 240.00, image: ['buty_adidas'] },
+      { id: 2, name: "Kurtka sportowa", category: "kurtki", description: "Chroni przed deszczem", price: 250.99, image: ['kurtka_adidas'] },
+      { id: 3, name: "Spodenki", category: "Spodnie", description: "Stworzone z miękkiego materiału", price: 109.50, image: ['spodnie'] },
+      { id: 4, name: "piłka", category: "Spodnie", description: "Stworzone z miękkiego materiału", price: 109.50, image: ['spodnie'] }
       ],
       orders: []
   }
