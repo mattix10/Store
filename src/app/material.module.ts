20daec2f2d0aafeee2db3a2b17cd1daa168dbfe2
Mati <mattigravity9@gmail.com>
2020-08-18 09:48:56 +0200

    join MatIconModule and MatBadgeModule to material.module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,13 +5,16 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatListModule } from '@angular/material/list';
 import { MatRippleModule } from '@angular/material/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [ CommonModule, MatButtonModule, MatGridListModule, MatListModule, MatRippleModule,
-             MatCardModule],
+             MatCardModule, MatBadgeModule, MatIconModule],
   declarations: [
   ],
-  exports: [MatButtonModule, MatGridListModule, MatListModule, MatRippleModule, MatCardModule]
+  exports: [ MatButtonModule, MatGridListModule, MatListModule, MatRippleModule, MatCardModule,
+             MatBadgeModule, MatIconModule]
 })
 
 export class MaterialModule { }
