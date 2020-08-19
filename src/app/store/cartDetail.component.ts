9af4da2ae8c33f9f8c7c56c8061e93eecca38f6d
Mati <mattigravity9@gmail.com>
2020-08-19 12:23:01 +0200

    feat: add styleUrls

--- src/app/store/cartDetail.component.ts
+++ src/app/store/cartDetail.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 import { Cart } from '../model/cart.model';
 
 @Component({
-  templateUrl: 'cartDetail.component.html'
+  templateUrl: 'cartDetail.component.html',
+  styleUrls: ['cartDetail.component.css']
 })
 
 export class CartDetailComponent {
+
   constructor(public cart: Cart) {}
+
 }
