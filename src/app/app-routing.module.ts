71d526f93fb0c80359f80ec7aca28e198a64b8ce
Mati <mattigravity9@gmail.com>
2020-08-20 22:38:33 +0200

    feat: change Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { StoreComponent } from './store/store.component';
 import { CheckoutComponent } from './store/checkout.component';
 import { CartDetailComponent } from './store/cartDetail.component';
+import { ProductDetailComponent } from './store/productDetail.component';
 import { ModelResolver } from './model/model.resolver';
+import { MainComponent } from './store/main.component';
 
 
-const routes: Routes = [
-  { path: 'store', component: StoreComponent, resolve: { model: ModelResolver}},
+const childRoutes: Routes = [
+  
+  { path: 'product/:id', component: ProductDetailComponent},
   { path: 'cart', component: CartDetailComponent },
+  { path: 'category/:category', component: StoreComponent},
   { path: 'checkout', component: CheckoutComponent },
-  { path: 'admin', loadChildren:  () => import('src/app/admin/admin.module').then(mod => mod.AdminModule)},
+  { path: '', component: StoreComponent},
+  { path: 'admin', loadChildren:  () => import('src/app/admin/admin.module').then(mod => mod.AdminModule)}
+];
+const routes: Routes = [
+  { path: 'store', component: MainComponent, children: childRoutes },
   { path: '**', redirectTo: '/store' }
 ];
 
