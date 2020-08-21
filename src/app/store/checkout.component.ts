a9b97d0df12d113c6c69e812e1f6b1d31713a3f0
Mati <mattigravity9@gmail.com>
2020-08-21 15:09:16 +0200

    feat: add states

--- src/app/store/checkout.component.ts
+++ src/app/store/checkout.component.ts
@@ -5,14 +5,35 @@ import { OrderRepository } from '../model/order.repository';
 
 
 @Component({
-  templateUrl: 'checkout.component.html'
+  templateUrl: 'checkout.component.html',
+  styleUrls: ['checkout.component.css']
 })
 
 export class CheckoutComponent {
   orderSent = false;
   submitted = false;
 
-  constructor(public repository: OrderRepository, public order: Order) {}
+  constructor(public repository: OrderRepository,
+              public order: Order) {}
+
+  states: string[] = [
+    'dolnośląskie',
+    'kujawsko-pomorskie',
+    'lubelskie',
+    'lubuskie',
+    'łódzkie',
+    'małopolskie',
+    'mazowieckie',
+    'opolskie',
+    'podarpackie',
+    'podlaskie',
+    'pomorskie',
+    'śląskie',
+    'świętokrzyskie',
+    'warmińsko-mazurskie',
+    'wielkopolskie',
+    'zachodniopomorskie'
+    ];
 
   submitOrder(form: NgForm) {
     this.submitted = true;
