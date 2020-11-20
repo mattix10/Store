2b9683d6272e25317e46706a3fab6ecf50f25109
mattix10 <mateusz.salamon10@gmail.com>
2020-11-20 16:01:12 +0100

    change directory

---
+++ src/app/store/productDetail/productDetail.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Product } from '../../model/product.model';
+import { ProductRepository } from '../../model/product.repository';
+import { ActivatedRoute } from '@angular/router';
+import { Cart } from '../../model/cart.model';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: 'productDetail.component.html',
+  styleUrls: ['productDetail.component.css']
+})
+
+export class ProductDetailComponent {
+  product: Product = new Product();
+
+  constructor(private repository: ProductRepository, activeRoute: ActivatedRoute,
+              private cart: Cart) {
+    const id = activeRoute.snapshot.params.id;
+    if (id !== null) {
+      Object.assign(this.product, repository.getProduct(id));
+    }
+  }
+
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
+  }
+}
