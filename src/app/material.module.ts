cc9281ddd036c207beafa10c9d5fadf49c27e613
mattix10 <mateusz.salamon10@gmail.com>
2020-11-20 11:41:39 +0100

    add some MaterialModules

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -10,15 +10,24 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   imports: [ CommonModule, MatButtonModule, MatGridListModule, MatListModule, MatRippleModule,
              MatCardModule, MatBadgeModule, MatIconModule, MatFormFieldModule, MatInputModule,
-             MatSelectModule],
+             MatSelectModule, MatSidenavModule, MatExpansionModule, MatTableModule, MatPaginatorModule, MatSortModule,
+             MatProgressSpinnerModule],
   declarations: [
   ],
   exports: [ MatButtonModule, MatGridListModule, MatListModule, MatRippleModule, MatCardModule,
-             MatBadgeModule, MatIconModule, MatFormFieldModule, MatInputModule, MatSelectModule]
+             MatBadgeModule, MatIconModule, MatFormFieldModule, MatInputModule, MatSelectModule,
+             MatSidenavModule, MatExpansionModule, MatTableModule, MatPaginatorModule, MatSortModule, 
+             MatProgressSpinnerModule]
 })
 
 export class MaterialModule { }
