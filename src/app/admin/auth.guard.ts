be5046f7b23d01ebee75bd04c5e097e6ccae5d4f
mattix10 <mateusz.salamon10@gmail.com>
2020-11-20 14:05:42 +0100

    fix: changed activate's condition

--- src/app/admin/auth.guard.ts
+++ src/app/admin/auth.guard.ts
@@ -7,12 +7,12 @@ export class AuthGuard {
   constructor(private router: Router,
               private auth: AuthService) {}
 
-  canActivate(route: ActivatedRouteSnapshot,
-              state: RouterStateSnapshot): boolean {
-      if (!this.auth.authenticated) {
-        this.router.navigateByUrl('/admin/auth');
+  canActivate(): boolean {
+      if (this.auth.loggedIn()) {
+        return true;
+      } else {
+        this.router.navigate(['/auth']);
         return false;
       }
-      return true;
     }
 }
