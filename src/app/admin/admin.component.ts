e71895135a2c7453caf7975932654f0246779119
mattix10 <mateusz.salamon10@gmail.com>
2020-11-25 13:56:38 +0100

    remove imports and method: logout()

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,17 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../model/auth.service';
 
 @Component({
-  templateUrl: 'admin.component.html',
-  styleUrls: ['admin.component.css']
+  templateUrl: 'admin.component.html'
 })
-export class AdminComponent {
-  constructor(private auth: AuthService,
-              private router: Router) {}
-  
-  logout() {
-    this.auth.clear();
-    this.router.navigateByUrl('/');
-  }
-}
+export class AdminComponent {}
