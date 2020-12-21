cb1e902c049c4110912da18f47f03c74f706fde8
mattix10 <mateusz.salamon10@gmail.com>
2020-12-22 00:21:20 +0100

    feat: add refresh, closeModal, editProduct methods

--- src/app/admin/tables/productTable.component.ts
+++ src/app/admin/tables/productTable.component.ts
@@ -1,38 +1,58 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductRepository } from '../../model/product.repository';
 import { Product } from '../../model/product.model';
 import { ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import {MatTable, MatTableDataSource} from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { RestDataSource } from '../../model/rest.datasource';
-import { pipe } from 'rxjs';
 
 @Component({
   templateUrl: 'productTable.component.html',
   styleUrls: ['table.component.css']
 })
+
 export class ProductTableComponent implements OnInit{
-  dataSource: any;
-  pageSizeOptions: number;
-  products: Array<any>;
+  
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatTable) tableReference: MatTable<any>;
+  
+  public dataSource: any;
+  public pageSizeOptions: number;
+  public products: Array<any>;
+  public displayedColumns: string[] = ['name', 'category', 'description', 'price', 'id'];
+  public selectedProduct;
+  public openModal = false;
+  public choosenProduct;
 
-  constructor(private repository: ProductRepository, private restDataSource: RestDataSource) {
-  }
-  displayedColumns: string[] = ['name', 'category', 'description', 'price', 'id'];
-  selectedProduct;
+  constructor(private repository: ProductRepository, private restDataSource: RestDataSource) {}
 
   ngOnInit(): void {
-   this.restDataSource.getProducts().subscribe( res => {
+    this.refresh();
+  }
+
+  refresh() {
+    this.restDataSource.getProducts().subscribe( res => {
       this.products = res;
       this.dataSource = new MatTableDataSource<any>(this.products);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
-      // this.tableReference.renderRows();
     });
+  }
+
+  closeModal(openModal: boolean): void {
+    this.openModal = openModal;
+    this.refresh();
+  }
+
+  editProduct(id: string): void {
+    this.openModal = true;
+    this.choosenProduct =  this.products.find(p => p._id == id);
+  }
+
+  getProduct(id: string): Product {
+    return this.products.find(p => p._id == id);
   }
   
   getProducts(): Product[] {
@@ -41,9 +61,10 @@ export class ProductTableComponent implements OnInit{
 
   deleteProduct(id: number): void {
     this.repository.deleteProduct(id);
+    this.refresh();
   }
 
-  getSelected(product): boolean {
+  getSelected(product: Product): boolean {
     return product.name == this.selectedProduct;
   }
 }
