aaf962956cc18e205b0ac1ed41a32ca2496b98d4
mattix10 <mateusz.salamon10@gmail.com>
2021-01-05 23:09:21 +0100

    refactor: divide code into routes

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,88 +1,27 @@
 const express = require('express');
-const app = express();
-const mongoose  = require('./mongoose')
+const mongoose  = require('./mongoose');
 const bodyParser = require('body-parser');
-const Product = require('../models/products.model');
-const config = require ('./dbconfig');
-const api = require('./api');
-const Order = require('../models/orders.model');
-const { db } = require('./dbconfig');
 const dbconfig = require('./dbconfig');
-const { text } = require('express');
+const { db } = require('./dbconfig');
+const Product = require('../models/products.model');
+const Order = require('../models/orders.model');
+const config = require ('./dbconfig');
+const app = express();
+const productRouter = require('../routes/product.routes');
+const orderRouter = require('../routes/order.routes');
+const storeRouter = require('../routes/store.routes');
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header('Access-Control-Allow-Methods', 'PUT, PATCHT, POST, GET, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'PUT, PATCH, POST, GET, DELETE, OPTIONS');
   next();
 });
-
 app.use(bodyParser.json());
-app.use('/api', api);
-app.get('/products', (req, res) => {
-  Product.find({}, (err, data) => {
-    res.send(data);
-  })
-})
 
-app.post('/products', (req, res) => {
-
-  let name = req.body.name;
-  let newProduct = new Product({
-    name
-  })
-  newProduct.save().then((listDoc) => {
-    res.send(listDoc);
-  })
-})
-
-app.get('/store', (req, res) => {
-
-  productName = req.query.productName;
-  searchText = {$text: { $search: productName}}
-  query = { name: searchText};
-  Product.find(example, (err, data) => {
-    Product.create
-    res.send(data);
-  })
-})
-
-app.delete('/products/:id', (req, res) => {
-
-  let params =  req.params;
-  Product.findOneAndRemove({
-    _id: params.id
-  }).then((removedProductDoc) => {
-    res.send(removedProductDoc);
-    console.log(removedProductDoc)
-  })
-})
-
-app.patch('/products/:id', (req, res) => {
-
-  Product.findOneAndUpdate({ _id: req.params.id}, {
-    $set: req.body
-  }).then(() => {
-    res.sendStatus(200);
-  });
-})
-
-app.post('/orders', (req, res) => {
-
-  let newOrder = new Order({
-    name: req.body.name,
-    address: req.body.address,
-    city: req.body.city,
-    state: req.body.state,
-    zip: req.body.name,
-    country: req.body.coutntry,
-    shipped: req.body.shipped,
-    cart: req.body.cart
-  })
-  newOrder.save().then((orders) => {
-    res.send(orders);
-  })
-})
+app.use('/', storeRouter);
+app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
 
 app.listen(3000, () => {
   console.log('server is listening at port 3000')
