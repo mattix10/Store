8bcc31f8ce3f9d95af0f0042d2778244f5f4428a
mattix10 <mateusz.salamon10@gmail.com>
2021-01-05 23:03:06 +0100

    add file: auth.controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,38 @@
+const User = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+
+exports.verificationJWT = async (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send('Unauthorized request');
+  }
+  let token = req.headers.authorization.split(' ')[1]
+  if (token === 'null') {
+    return res.status(401).send('Unauhtorized request');
+  }
+  const payload = jwt.verify(token, 'secret');
+  if (!payload) {
+    return res.status(401).send('Unauthorized request');
+  }
+  next()
+}
+
+exports.login = async (req, res) => {
+  const {email, password} = req.body;
+
+  User.findOne({email: email}, (error, user) => {
+    if (error) {
+      console.log(error);
+    } else {
+      if (!user) {
+        res.status(401).send('Invalid email');
+        } else 
+            if (user.password !== password) {
+                res.status(401).send('Invalid password');
+            } else {
+                const payload = {subject: user._id};
+                const token = jwt.sign(payload, 'secret');
+                res.status(200).send({token});
+            }
+        }
+    })
+}
