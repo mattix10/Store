331475c5f7414fba7d1eba631d4d45c954d01b91
mattix10 <mateusz.salamon10@gmail.com>
2021-02-08 20:15:44 +0100

    refactor: add indents

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,28 +1,34 @@
-const express = require('express');
-const mongoose  = require('./mongoose');
-const bodyParser = require('body-parser');
-const dbconfig = require('./dbconfig');
-const { db } = require('./dbconfig');
-const Product = require('../models/products.model');
-const Order = require('../models/orders.model');
-const config = require ('./dbconfig');
+const express = require("express");
+const mongoose = require("./mongoose");
+const bodyParser = require("body-parser");
+const dbconfig = require("./dbconfig");
+const { db } = require("./dbconfig");
+const Product = require("../models/products.model");
+const Order = require("../models/orders.model");
+const config = require("./dbconfig");
 const app = express();
-const productRouter = require('../routes/product.routes');
-const orderRouter = require('../routes/order.routes');
-const storeRouter = require('../routes/store.routes');
+const productRouter = require("../routes/product.routes");
+const orderRouter = require("../routes/order.routes");
+const storeRouter = require("../routes/store.routes");
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header('Access-Control-Allow-Methods', 'PUT, PATCH, POST, GET, DELETE, OPTIONS');
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "PUT, PATCH, POST, GET, DELETE, OPTIONS"
+  );
   next();
 });
 app.use(bodyParser.json());
 
-app.use('/', storeRouter);
-app.use('/api/products', productRouter);
-app.use('/api/orders', orderRouter);
+app.use("/", storeRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 
 app.listen(3000, () => {
-  console.log('server is listening at port 3000')
-})
+  console.log("server is listening at port 3000");
+});
--- backend/server/mongoose.js
+++ backend/server/mongoose.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const config = require('./dbconfig');
+
 mongoose.Promise = global.Promise;
-mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => { console.log('connected successfully');
-}).catch((e) => {
-  console.log('Error');
-  console.log(e);
+mongoose.connect(config.db, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true})
+  .then(() => { console.log('connected successfully') })
+  .catch((e) => {
+    console.log('Error');
+    console.log(e);
 })
 
 mongoose.set('useCreateIndex', true);
