686e50acb22007672da707d33d99e5342517083e
mattix10 <mateusz.salamon10@gmail.com>
2021-04-23 16:09:14 +0200

    add helmet and xss packages

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const helmet = require("helmet");
+const xss = require("xss-clean");
 const mongoose = require("./server");
 const productRouter = require("../routes/product.routes");
 const orderRouter = require("../routes/order.routes");
 const storeRouter = require("../routes/store.routes");
 
 const app = express();
+
+app.use(hemlet());
+app.use(xss());
 app.use(express.json());
 dotenv.config({ path: "./config.env" });
 
