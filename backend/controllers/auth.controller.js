ce04bea57597a6b6ef142d1b32485e798338b4b2
mattix10 <mateusz.salamon10@gmail.com>
2021-04-23 16:21:47 +0200

    fix: shorten login condition

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,38 +1,35 @@
-const User = require('../models/user.model');
-const jwt = require('jsonwebtoken');
+const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
 exports.verificationJWT = async (req, res, next) => {
   if (!req.headers.authorization) {
-    return res.status(401).send('Unauthorized request');
+    return res.status(401).send("Unauthorized request");
   }
-  let token = req.headers.authorization.split(' ')[1]
-  if (token === 'null') {
-    return res.status(401).send('Unauhtorized request');
+  let token = req.headers.authorization.split(" ")[1];
+  if (token === "null") {
+    return res.status(401).send("Unauhtorized request");
   }
-  const payload = jwt.verify(token, 'secret');
+  const payload = jwt.verify(token, "secret");
   if (!payload) {
-    return res.status(401).send('Unauthorized request');
+    return res.status(401).send("Unauthorized request");
   }
-  next()
-}
+  next();
+};
 
 exports.login = async (req, res) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
 
-  User.findOne({email: email}, (error, user) => {
+  User.findOne({ email: email }, (error, user) => {
     if (error) {
-      console.log(error);
+      console.error(error.name, error.message);
     } else {
       if (!user) {
-        res.status(401).send('Invalid email');
-        } else 
-            if (user.password !== password) {
-                res.status(401).send('Invalid password');
-            } else {
-                const payload = {subject: user._id};
-                const token = jwt.sign(payload, 'secret');
-                res.status(200).send({token});
-            }
-        }
-    })
-}
+        res.status(401).send("Invalid email or password");
+      } else {
+        const payload = { subject: user._id };
+        const token = jwt.sign(payload, "secret");
+        res.status(200).send({ token });
+      }
+    }
+  });
+};
